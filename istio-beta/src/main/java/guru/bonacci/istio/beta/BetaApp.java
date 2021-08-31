391edbb3b55834c7694c0ddab5f1041e6cd0b9f9
Jeffrey van Helden <311759@twgroup.co.nz>
2021-09-01 10:52:44 +1200

    sleuth header propagation

--- istio-beta/src/main/java/guru/bonacci/istio/beta/BetaApp.java
+++ istio-beta/src/main/java/guru/bonacci/istio/beta/BetaApp.java
@@ -2,23 +2,11 @@ package guru.bonacci.istio.beta;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BetaApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BetaApp.class, args);
-	}
-
-
-	@Configuration
-	public class WebConfiguration extends WebMvcConfigurerAdapter {
-		@Override
-		public void addInterceptors(InterceptorRegistry registry) {
-			registry.addInterceptor(new LogHeadersInterceptor());
-		}
 	}
 }
